d0152514e8c03b4b791d45266c61e1126977b17d
XHC <2445917342@qq.com>
2023-08-02 14:00:58 +0800

    第一次改

---
+++ Assets/后加的Scripts/ADDTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ADDTest : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        gameObject.name = "Xinhancheng";
+    }
+
+    void XinHanCheng()
+    {
+
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(5f);
+
+        int a =2;
+        int b = 3;
+        int c = a + b;
+        Debug.Log(c);
+
+    }
+ 
+}
